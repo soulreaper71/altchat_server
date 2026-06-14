ec07aeff0cdf1aab41c95d7dae9722b979e5148a
soulreaper71 <soulreaperbs07@gmail.com>
2026-06-14 22:14:29 +0530

    update

--- server.py
+++ server.py
@@ -7,20 +7,20 @@ server_connection_manager  = socket.socket()
 
 port = 6767
 
-server.bind(("",port))
+server_connection_manager.bind(("",port))
 print("socket binded to %s" %(port))
 
-server.listen(50)
+server_connection_manager.listen(50)
 print("socket is listening")
 
-while true:
+while True:
 
-	c, addr = server.accept()
+	c, addr = server_connection_manager.accept()
 
 	print("received connection from: ", adrr)
 
-	c.send("Thank you for connecting".encode())
-
+	c.send("You are currently connected to altchat messaging service... Thank You for connecting... your".encode())
+	c.send("Your connection was terminated...".decode())
 	c.close()
 
 	break
