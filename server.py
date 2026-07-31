05dcbc4e93c582c0b707c624ee4a374dc37be3c3
soulreaper71 <soulreaperbs07@gmail.com>
2026-07-31 22:12:17 +0530

    update

--- server.py
+++ server.py
@@ -1,25 +1,7 @@
-import requests
-import urllib3
 import socket
 
-server_connection_manager  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class connection_handler: 
 
-port = 6971
+	def __init__(self,):
 
-server_connection_manager.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_connection_manager.bind(("",port))
-print("socket binded to %s" %(port))
-
-server_connection_manager.listen(50)
-print("socket is listening")
-
-while True:
-	c, addr = server_connection_manager.accept()
-	if addr == "127.0.0.1":
-		c.close()
-	else:
-		print("received connection from: ", addr)
-		c.send("You are currently connected to altchat messaging service... Thank You for connecting... your".encode())
-		c.send("Your connection was terminated...".encode())
-		c.close()
-		break		
+		
