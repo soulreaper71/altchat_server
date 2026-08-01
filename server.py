3913018f8b7b11f637e75e2271c6a20b755ee5de
soulreaper71 <soulreaperbs07@gmail.com>
2026-08-01 18:46:44 +0530

    update

--- server.py
+++ server.py
@@ -1,18 +1,6 @@
-import socket
-
-class connection_handler: 
-
-	tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-	def __init__(self):
+import connection_handler
 
 
-	def connect():
+while 1:
+	connection_handler.connection_handler.connect()
 
-		tcp_socket.bind('',4443)
-		tcp_socket.listen(7000)
-		
-		while 1: 
-			client,addr = tcp_socket.accept()
-			client.send("Hello. Brave Traveller.", )
-			client.send("We will terminate this connection now.")
