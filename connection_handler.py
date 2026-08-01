5bf9c59d95b012fe7c1285274c453d878f2e1bf2
soulreaper71 <soulreaperbs07@gmail.com>
2026-08-01 19:33:23 +0530

    test

--- connection_handler.py
+++ connection_handler.py
@@ -11,8 +11,11 @@ class connection_handler:
 		tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)	
 		tcp_socket.bind(('',443))
 		tcp_socket.listen(7000)
+		print ("socket is listening")
 		
 		while 1: 
 			client,addr = tcp_socket.accept()
+			print ('Got connection from', addr )
 			client.send("Hello. Brave Traveller.".encode() )
 			client.send("We will terminate this connection now.".encode())
+
