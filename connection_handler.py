d65962495724b55f3bd9d9df9e0c8f9272f813ac
soulreaper71 <soulreaperbs07@gmail.com>
2026-08-01 19:18:47 +0530

    first test

--- connection_handler.py
+++ connection_handler.py
@@ -2,13 +2,13 @@ import socket
 
 class connection_handler: 
 
-	tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+	
 	def __init__(self):
 		pass
 
 	def connect():
-		
+
+		tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)	
 		tcp_socket.bind('',443)
 		tcp_socket.listen(7000)
 		
